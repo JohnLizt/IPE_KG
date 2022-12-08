63326fbd66a5f0ec9ab850e2e1b704dd07dcc5b8
apple <apple@appledeMacBook-Pro.local>
2022-12-08 15:34:22 +0800

    完成CRUD及前端

--- Model/neo4j_models.py
+++ Model/neo4j_models.py
@@ -69,35 +69,55 @@ class Neo4j_Handle():
 
     # 关系查询：实体1+实体2(注意Entity2的空格）
     def findRelationByEntities(self, entity1, entity2):
-        # 品牌 + 品牌
-        answer = self.graph.run(
-            "MATCH (n1:Bank {name:\"" + entity1 + "\"})- [rel] -> (n2:Bank{name:\"" + entity2 + " \"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            # 品牌 + 系列
-            answer = self.graph.run(
-                "MATCH (n1:Bank {name:\"" + entity1 + "\"})- [rel] -> (n2:Serise{name:\"" + entity2 + " \"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            # 系列 + 品牌
-            answer = self.graph.run(
-                "MATCH (n1:Serise {name:\"" + entity1 + "\"})- [rel] -> (n2:Bank{name:\"" + entity2 + " \"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            # 系列 + 系列
-            answer = self.graph.run(
-                "MATCH (n1:Serise {name:\"" + entity1 + "\"})- [rel] -> (n2:Serise{name:\"" + entity2 + " \"}) RETURN n1,rel,n2").data()
+        answer = self.graph.run("MATCH (n1 {name:\"" + entity1 + "\"})- [rel] -> (n2 {name:\"" + entity2 + "\"}) "
+                                                                                                           "RETURN n1,rel,n2").data()
         return answer
 
     # 查询数据库中是否有对应的实体-关系匹配
-    def findEntityRelation(self, entity1, relation, entity2):
+    def findEntityRelation(self, entity1, entity2, relation):
         answer = self.graph.run(
-            "MATCH (n1:Bank {name:\"" + entity1 + "\"})- [rel:subbank {type:\"" + relation + "\"}] -> (n2:Bank{name:\"" + entity2 + "\"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            answer = self.graph.run(
-                "MATCH (n1:Bank {name:\"" + entity1 + "\"})- [rel:subbank {type:\"" + relation + "\"}] -> (n2:Serise{name:\"" + entity2 + "\"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            answer = self.graph.run(
-                "MATCH (n1:Serise {name:\"" + entity1 + "\"})- [rel:subbank {type:\"" + relation + "\"}] -> (n2:Bank{name:\"" + entity2 + "\"}) RETURN n1,rel,n2").data()
-        if (len(answer) == 0):
-            answer = self.graph.run(
-                "MATCH (n1:Serise {name:\"" + entity1 + "\"})- [rel:subbank {type:\"" + relation + "\"}] -> (n2:Serise{name:\"" + entity2 + "\"}) RETURN n1,rel,n2").data()
-
+            "MATCH (n1 {name:\"" + entity1 + "\"})- [rel {type:\"" + relation + "\"}] -> (n2{name:\"" + entity2 + "\"}) RETURN n1,rel,n2").data()
         return answer
+
+    # 新建实体
+    def createEntity(self, entityName, entityTag, entityRelease, entityLink):
+        answer = []
+        try:
+            if entityTag == "IPE_l0":
+                answer = self.graph.run(
+                    "CREATE (n1:" + entityTag + "{name:\"" + entityName + "\", released:\"" + entityRelease + "\", link:\"" + entityLink + "\"}) RETURN n1").data()
+            elif entityTag == "IPE_l1" or entityTag == "IPE_l2":
+                answer = self.graph.run(
+                    "CREATE (n1:" + entityTag + "{name:\"" + entityName + "\", released:\"" + entityRelease + "\"}) RETURN n1").data()
+            else:
+                answer = self.graph.run(
+                    "CREATE (n1:" + entityTag + "{name:\"" + entityName + "\"}) RETURN n1").data()
+        finally:
+            return answer
+
+    # 删除实体
+    def deleteEntity(self, entityName):
+        try:
+            self.graph.run("MATCH (n {name:\"" + entityName + "\"}) DETACH DELETE n")
+            return True
+        except:
+            return False
+
+    # 创建两个已存在实体间的关系
+    def createRel(self, entity1, entity2, relation):
+        try:
+            cypher = "MATCH (n1 {name:\"" + entity1 + "\"}) MATCH (n2 {name:\"" + entity2 + "\"}) CREATE (n1)-[:" + relation + "]->(n2)"
+            print(cypher)
+            self.graph.run(
+                "MATCH (n1 {name:\"" + entity1 + "\"}) MATCH (n2 {name:\"" + entity2 + "\"}) CREATE (n1)-[:" + relation + "]->(n2)")
+            return True
+        except:
+            return False
+
+    # 删除两个实体间的关系
+    def deleteRel(self, entity1, entity2):
+        try:
+            self.graph.run("MATCH (n1 {name:\"" + entity1 + "\"}) - [r] - (n2 {name:\"" + entity2 + "\"}) DELETE r")
+            return True
+        except:
+            return False
--- kgcar/relation_view.py
+++ kgcar/relation_view.py
@@ -49,60 +49,101 @@ def search_entity(request):
 			for i in range(len(entityRelation)):
 				relJsonData[i]['rel'] = entityRelation[i]["rel"].__class__.__name__
 			return render(request,'entity.html',{'entityRelation':json.dumps(relJsonData)})
+
 	#需要进行类型转换
 	return render(request,"entity.html",{'ctx':ctx})
 
+# 实体及关系管理
 def search_relation(request):
 	ctx = {}
 	if(request.GET):
 		db = neo4jconn
-		entity1 = request.GET['entity1_text']
-		relation = request.GET['relation_name_text']
-		entity2 = request.GET['entity2_text']
-		relation = relation.lower()
-		searchResult = {}
-		#若只输入entity1,则输出与entity1有直接关系的实体和关系
-		if(len(entity1) != 0 and len(relation) == 0 and len(entity2) == 0):
-			searchResult = db.findRelationByEntity1(entity1)
-			# searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html',{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
+		# 处理实体管理请求
+		if len(request.GET) == 5:
 
-		#若只输入entity2则,则输出与entity2有直接关系的实体和关系
-		if(len(entity2) != 0 and len(relation) == 0 and len(entity1) == 0):
-			searchResult = db.findRelationByEntity2(entity2)
-			searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html',{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
-		#若输入entity1和relation，则输出与entity1具有relation关系的其他实体
-		if(len(entity1)!=0 and len(relation)!=0 and len(entity2) == 0):
-			searchResult = db.findOtherEntities(entity1,relation)
-			searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html',{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
-		#若输入entity2和relation，则输出与entity2具有relation关系的其他实体
-		if(len(entity2)!=0 and len(relation)!=0 and len(entity1) == 0):
-			searchResult = db.findOtherEntities2(entity2,relation)
-			searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html',{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
-		#若输入entity1和entity2,则输出entity1和entity2之间的关系
-		if(len(entity1) !=0 and len(relation) == 0 and len(entity2)!=0):
-			searchResult = db.findRelationByEntities(entity1,entity2)
-			searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html',{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
-		#若输入entity1,entity2和relation,则输出entity1、entity2是否具有相应的关系
-		if(len(entity1)!=0 and len(entity2)!=0 and len(relation)!=0):
-			print(relation)
-			searchResult = db.findEntityRelation(entity1,relation,entity2)
-			searchResult = sortDict(searchResult)
-			if(len(searchResult)>0):
-				return render(request,'relation.html'.encode("utf-8"),{'searchResult':json.dumps(searchResult,ensure_ascii=False)})
-		#全为空
-		if(len(entity1)!=0 and len(relation)!=0 and len(entity2)!=0 ):
-			pass
-		ctx= {'title' : '<h1>暂未找到相应的匹配</h1>'}
-		return render(request,'relation.html',{'ctx':ctx})
+			entityName = str.strip(request.GET['entityName'])
+			entityTag = str.strip(request.GET['entityTag'])
+			entityRelease = str.strip(request.GET['entityRelease'])
+			entityLink = str.strip(request.GET['entityLink'])
+			operation = str.strip(request.GET['entityOperation'])
+
+			# 新建实体
+			if operation == "createEntity":
+				if not entityName or not entityTag:
+					ctx = {'title': '新建实体失败：请填写实体名称和类型'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				if not entityRelease:
+					entityRelease = "null"
+				if not entityLink:
+					entityLink = "null"
+				answer = db.matchEntityItem(entityName)
+				if len(answer) != 0:
+					ctx = {'title': '新建实体失败：同名实体已存在'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				createResult = db.createEntity(entityName, entityTag, entityRelease, entityLink)
+				if (len(createResult) == 0):
+					ctx = {'title': '新建实体失败'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				else:
+					ctx = {'title': '新建实体成功'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+
+			# 删除实体
+			if operation == "deleteEntity":
+				if not entityName:
+					ctx = {'title': '删除实体失败：请填写实体名称'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				answer = db.matchEntityItem(entityName)
+				if len(answer) == 0:
+					ctx = {'title': '删除实体失败：实体不存在'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				deleteResult = db.deleteEntity(entityName)
+				if not deleteResult:
+					ctx = {'title': '删除实体失败'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				else:
+					ctx = {'title': '删除实体成功'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+
+
+		# 处理关系管理请求
+		if len(request.GET) == 4:
+			entity1_text = str.strip(request.GET['entity1_text'])
+			operation = str.strip(request.GET['relOperation'])
+			entity2_text = str.strip(request.GET['entity2_text'])
+			relation = str.strip(request.GET['relType'])
+
+			if not entity1_text or not entity2_text:
+				ctx = {'title': '请填写实体名称'}
+				return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+			entity1 = db.matchEntityItem(entity1_text)
+			entity2 = db.matchEntityItem(entity2_text)
+			if not entity1 or not entity2:
+				ctx = {'title': '实体不存在'}
+				return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+			if operation == "createRel":
+				answer = db.findEntityRelation(entity1_text, entity2_text, relation)
+				if len(answer) > 0:
+					ctx = {'title': '关系创建失败：关系已存在'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				db.deleteRel(entity1_text,entity2_text)
+				createRes = db.createRel(entity1_text, entity2_text, relation)
+				opposite = {"包含": "属于", "属于": "包含", "关联": "关联"}
+				createRes = createRes and db.createRel(entity2_text,entity1_text, opposite[relation])
+				if createRes is True:
+					ctx = {'title': '关系创建成功'}
+				else:
+					ctx = {'title': '关系创建成功'}
+			if operation == "deleteRel":
+				answer = db.findEntityRelation(entity1_text, entity2_text, relation)
+				if len(answer) == 0:
+					ctx = {'title': '关系删除失败：关系不存在'}
+					return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
+				delRes = db.deleteRel(entity1_text, entity2_text)
+				if delRes is True:
+					ctx = {'title': '关系删除成功'}
+				else:
+					ctx = {'title': '关系删除失败'}
+			return render(request, 'relation.html', {'ctx': json.dumps(ctx, ensure_ascii=False)})
 
 	return render(request, 'relation.html', {'ctx':ctx})
